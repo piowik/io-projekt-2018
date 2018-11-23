69caceb86eedd248d703d955f2adf41ac339d1e5
AMaczynski <artur.m1717@gmail.com>
2018-11-23 21:39:00 +0100

    LoginActivity divided into fragments, communication between them done, design 50%

---
+++ app/src/main/java/io/almp/flatmanager/LoginFragment.java
@@ -0,0 +1,116 @@
+package io.almp.flatmanager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private Button signUp;
+    private Button signIn;
+    private TextView email;
+    private TextView password;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        email = rootView.findViewById(R.id.email);
+        password = rootView.findViewById(R.id.password);
+        signUp = rootView.findViewById(R.id.sign_up);
+        signUp.setOnClickListener(v->{
+            String emailS;
+            String passwordS;
+            emailS = email.getText().toString();
+            passwordS = password.getText().toString();
+
+            Bundle bundle = new Bundle();
+            bundle.putString("email", emailS);
+            bundle.putString("password", passwordS);
+
+            SignUpFragment signUpFragment = new SignUpFragment();
+            signUpFragment.setArguments(bundle);
+
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, signUpFragment);
+            onSaveInstanceState(savedInstanceState);
+            fragmentTransaction.commit();
+
+        });
+        signIn = rootView.findViewById(R.id.sign_in);
+        signIn.setOnClickListener(v->{
+            //TODO everything to login
+        });
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/io/almp/flatmanager/SignUpFragment.java
@@ -0,0 +1,99 @@
+package io.almp.flatmanager;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SignUpFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SignUpFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SignUpFragment extends Fragment {
+    private EditText email;
+    private EditText password;
+    private OnFragmentInteractionListener mListener;
+    private Button signUp;
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SignUpFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SignUpFragment newInstance(String param1, String param2) {
+        SignUpFragment fragment = new SignUpFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        String emailS = getArguments().getString("email");
+        String passwordS = getArguments().getString("password");
+        System.out.println(emailS + ", " + passwordS);
+        email = rootView.findViewById(R.id.email_2);
+        email.setText(emailS);
+        password = rootView.findViewById(R.id.password_2);
+        password.setText(passwordS);
+
+        signUp = rootView.findViewById(R.id.sign_up_2);
+        signUp.setOnClickListener(v->{
+            //TODO register user
+        });
+
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
