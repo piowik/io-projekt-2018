b55cead5dfa2997c485b28ccc118efea45d839c6
Piotr <piotrwiklinski@gmail.com>
2018-11-28 10:22:00 +0100

    Notifications done right: show title and msg, ability to include image

--- app/src/main/java/io/almp/flatmanager/service/FirebaseMessagingService.java
+++ app/src/main/java/io/almp/flatmanager/service/FirebaseMessagingService.java
@@ -5,17 +5,26 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.text.Html;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import io.almp.flatmanager.ChatActivity;
 import io.almp.flatmanager.R;
-
-import static android.content.ContentValues.TAG;
+import io.almp.flatmanager.RentActivity;
 
 /**
  * Created by Mateusz Zaremba on 27.11.2018.
@@ -23,37 +32,89 @@ import static android.content.ContentValues.TAG;
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
 
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        showNotification(remoteMessage.getData().get("message"));
+        if (remoteMessage.getData().size() > 0) {
+            Log.e("Got data:", remoteMessage.getData().toString() + "");
+            String title = remoteMessage.getData().get("title");
+            String body = remoteMessage.getData().get("message");
+            String imageUrl = remoteMessage.getData().get("image");
+            if (!TextUtils.isEmpty(imageUrl)) {
+                Log.e("Image not empty","not");
+                if (imageUrl != null && imageUrl.length() > 4 && Patterns.WEB_URL.matcher(imageUrl).matches()) {
+                    Bitmap bitmap = getBitmapFromURL(imageUrl);
+                    if (bitmap != null)
+                        showImageNotification(bitmap, title, body);
+                    else
+                        showNotification(title, body);
+                }
+            }
+            else
+                showNotification(title, body);
+
+            Log.e("DataTitle", title);
+
+        }
+        if (remoteMessage.getNotification() != null) {
+            Log.e("NotificationTitle", "T:" + remoteMessage.getNotification().getTitle());
+            String title = remoteMessage.getNotification().getTitle();
+            String body = remoteMessage.getNotification().getBody();
+            showNotification(title, body);
+        }
+        //getting the title and the body
+
     }
 
     @Override
     public void onNewToken(String token) {
         super.onNewToken(token);
         Log.e("newToken", token);
-        getSharedPreferences("_", MODE_PRIVATE).edit().putString("fb", token).apply();
+        getSharedPreferences("_", MODE_PRIVATE).edit().putString("fbtoken", token).apply();
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-       // sendRegistrationToServer(token);
+        // sendRegistrationToServer(token);
     }
 
-    private void showNotification(String message) {
+    private void showNotification(String title, String message) {
         createNotificationChannel();
-        Intent i = new Intent(this,ChatActivity.class);
+        Intent i = new Intent(this, RentActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "Chat")
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, getString(R.string.channel_id_rent))
                 .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                .setContentTitle("My notification")
-                .setContentText("Much longer text that cannot fit one line...")
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText("Much longer text that cannot fit one line..."))
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+//                .setStyle(new NotificationCompat.BigTextStyle()
+//                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setChannelId("Chat");
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, mBuilder.build());
+    }
+
+    private void showImageNotification(Bitmap bitmap, String title, String message) {
+        createNotificationChannel();
+        Intent i = new Intent(this, RentActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.BigPictureStyle bigPictureStyle = new NotificationCompat.BigPictureStyle();
+        bigPictureStyle.setBigContentTitle(title);
+        bigPictureStyle.setSummaryText(Html.fromHtml(message).toString());
+        bigPictureStyle.bigPicture(bitmap);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, getString(R.string.channel_id_rent))
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setStyle(bigPictureStyle)
+                .setContentIntent(pendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setChannelId("Chat");
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
@@ -67,10 +128,10 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
+            CharSequence name = getString(R.string.channel_name_rent);
+            String description = getString(R.string.channel_rent_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("Chat", name, importance);
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id_rent), name, importance);
             channel.setDescription(description);
             // Register the channel with the system; you can't change the importance
             // or other notification behaviors after this
@@ -79,8 +140,23 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         }
     }
 
+    public Bitmap getBitmapFromURL(String strURL) {
+        try {
+            URL url = new URL(strURL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     public static String getToken(Context context) {
-        return context.getSharedPreferences("_", MODE_PRIVATE).getString("fb", "empty");
+        return context.getSharedPreferences("_", MODE_PRIVATE).getString("fbtoken", "empty");
     }
 
 }
