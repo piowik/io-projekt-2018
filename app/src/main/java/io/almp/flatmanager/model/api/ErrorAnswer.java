44f483057a1ae8e65f07ef183fc446ff4ad356b0
Piotr <piotrwiklinski@gmail.com>
2018-12-20 14:36:35 +0100

    Simplified Error and SimpleError models for api calls

--- app/src/main/java/io/almp/flatmanager/model/api/ErrorAnswer.java
+++ app/src/main/java/io/almp/flatmanager/model/api/ErrorAnswer.java
@@ -1,13 +1,15 @@
 package io.almp.flatmanager.model.api;
 
 /**
- *  Class containing methods required to check and answer error if found.
+ * Class containing methods required to check and answer error if found.
  */
 
 
 public class ErrorAnswer {
 
     private boolean error;
+    private String message;
+
 
     public String getMessage() {
         return message;
@@ -17,27 +19,7 @@ public class ErrorAnswer {
         this.message = message;
     }
 
-    private String message;
-
-    public ErrorAnswer() {
-    }
-
-    /**
-     * @param error returns true if an error occurred
-     */
-    public ErrorAnswer(boolean error, String message) {
-        super();
-        this.error = error;
-        this.message=message;
-    }
-
     public boolean isError() {
         return error;
     }
-
-    public void setError(boolean error) {
-        this.error = error;
-    }
-
-
 }
--- app/src/main/java/io/almp/flatmanager/model/api/SimpleErrorAnswer.java
+++ app/src/main/java/io/almp/flatmanager/model/api/SimpleErrorAnswer.java
@@ -5,23 +5,9 @@ package io.almp.flatmanager.model.api;
  */
 
 public class SimpleErrorAnswer {
-
     private boolean error;
-
-    public SimpleErrorAnswer() {
-    }
-
-    /**
-     * @param error returns true if an error occurred
-     */
-    public SimpleErrorAnswer(boolean error) {
-        super();
-        this.error = error;
-    }
 
     public boolean isError() {
         return error;
     }
-
-
 }
