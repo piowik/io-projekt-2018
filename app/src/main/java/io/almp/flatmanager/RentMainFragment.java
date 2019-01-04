230cd0d683a49f9dc8500aa37c5319ee86e55f5c
Piotr <piotrwiklinski@gmail.com>
2019-01-04 13:46:23 +0100

    Splitted RentActivity into activity and fragment

---
+++ app/src/main/java/io/almp/flatmanager/RentMainFragment.java
@@ -0,0 +1,139 @@
+package io.almp.flatmanager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.almp.flatmanager.adapter.RentHistoryAdapter;
+import io.almp.flatmanager.model.RentHistoryItem;
+import io.almp.flatmanager.model.api.SimpleErrorAnswer;
+import io.almp.flatmanager.rest.ApiInterface;
+import io.almp.flatmanager.rest.ApiUtils;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * Main fragment for class of duties.
+ */
+
+public class RentMainFragment extends Fragment {
+    private RentHistoryAdapter mRentHistoryAdapter;
+    private ApiInterface mApiInterface;
+    private ImageButton sendRentButton;
+    private List<RentHistoryItem> mRentHistoryItemList;
+
+    public RentMainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_rent_main, container, false);
+        // mUserPoints = rootView.findViewById(R.id.all_flatmates_balances);
+
+        int flat_id = getContext().getSharedPreferences("_", MODE_PRIVATE).getInt("flat_id", 0);
+        long uid = getContext().getSharedPreferences("_", MODE_PRIVATE).getLong("user_id", 0L);
+        mApiInterface = ApiUtils.getAPIService();
+        mRentHistoryItemList = new ArrayList<>();
+        EditText rentValueEditText = rootView.findViewById(R.id.rentValueEditText);
+        ListView rentHistoryListView = rootView.findViewById(R.id.rentHistoryListView);
+        sendRentButton = rootView.findViewById(R.id.sendRentButton);
+        loadRents(flat_id);
+
+        sendRentButton.setOnClickListener(view -> {
+            if (TextUtils.isEmpty(rentValueEditText.getText().toString())) {
+                Toast.makeText(getContext(), R.string.rent_value_cannot_be_null, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            float rentValue = Float.valueOf(rentValueEditText.getText().toString());
+            sendPost(uid, flat_id, rentValue);
+            rentValueEditText.setText("");
+            sendRentButton.setEnabled(false);
+            loadRents(flat_id);
+        });
+        mRentHistoryAdapter = new RentHistoryAdapter(getActivity(), mRentHistoryItemList);
+        rentHistoryListView.setAdapter(mRentHistoryAdapter);
+        return rootView;
+    }
+
+
+    private void sendPost(long uid, int flat, float value) {
+        mApiInterface.sendRentData(uid, flat, value).enqueue(new Callback<SimpleErrorAnswer>() {
+            @Override
+            public void onResponse(Call<SimpleErrorAnswer> call, Response<SimpleErrorAnswer> response) {
+                Log.e("RespMsg", response.message() + "!");
+                Log.e("RespBody", response.toString() + "!");
+                if (response.isSuccessful()) {
+                    if (!response.body().isError()) {
+                        Log.e("POST", "Post submitted to API");
+
+                    } else {
+                        Toast toast = Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                } else {
+                    Toast toast = Toast.makeText(getContext(), getString(R.string.something_went_wrong), Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                sendRentButton.setEnabled(true);
+            }
+
+            @Override
+            public void onFailure(Call<SimpleErrorAnswer> call, Throwable t) {
+                Log.e("POST", "Unable to submit post to API.");
+                sendRentButton.setEnabled(true);
+            }
+        });
+    }
+
+    private void updateRents(List<RentHistoryItem> list) {
+        mRentHistoryItemList.clear();
+        mRentHistoryItemList.addAll(list);
+        mRentHistoryAdapter.notifyDataSetChanged();
+    }
+
+    private void loadRents(int flat) {
+        mApiInterface.getRents(flat).enqueue(new Callback<List<RentHistoryItem>>() {
+            @Override
+            public void onResponse(Call<List<RentHistoryItem>> call, Response<List<RentHistoryItem>> response) {
+                Log.e("RespMsg", response.message() + "!");
+                Log.e("RespBody", response.toString() + "!");
+                if (response.isSuccessful()) {
+                    List<RentHistoryItem> returnedList = response.body();
+                    updateRents(returnedList);
+                } else {
+                    Toast toast = Toast.makeText(getContext(), getString(R.string.something_went_wrong), Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                sendRentButton.setEnabled(true);
+            }
+
+            @Override
+            public void onFailure(Call<List<RentHistoryItem>> call, Throwable t) {
+                Log.e("POST", "Unable to submit post to API.");
+                sendRentButton.setEnabled(true);
+            }
+        });
+    }
+}
