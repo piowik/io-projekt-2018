7f1d5d8e95a4590302c7ceca93b7747ec07933f9
Piotr <piotrwiklinski@gmail.com>
2019-01-19 18:37:55 +0100

    AddRent tests

--- app/src/androidTest/java/io/almp/flatmanager/RentActivityTest.java
+++ app/src/androidTest/java/io/almp/flatmanager/RentActivityTest.java
@@ -10,11 +10,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 
 @RunWith(AndroidJUnit4.class)
@@ -32,9 +40,23 @@ public class RentActivityTest {
     }
 
     @Test
-    public void testCheckPossibilityToWriteValue() {
-        onView(withId(R.id.rentValueEditText)).perform(typeText("255.50"), closeSoftKeyboard());
-        onView(withId(R.id.sendRentButton)).perform(click());
+    public void testCheckPossibilityToWriteValue() throws InterruptedException {
+        onView(withId(R.id.button_rent_add)).perform(click());
+        Thread.sleep(5000);
+        for (int i = 0; i < CountHelper.getCountFromListUsingTypeSafeMatcher(R.id.listview_rent_per_user); i++) {
+            onData(anything()).inAdapterView(withId(R.id.listview_rent_per_user)).atPosition(i).onChildView(withId(R.id.rentValueEditText)).perform(typeText(String.valueOf(111 * (i + 1))), closeSoftKeyboard());
+        }
+        onView(withId(R.id.extrasEditText)).perform(typeText("1000"), closeSoftKeyboard());
+        onView(withId(R.id.button_rent_post)).perform(click());
+    }
+
+    @Test
+    public void testCheckEmptyValues() throws InterruptedException {
+        onView(withId(R.id.button_rent_add)).perform(click());
+        Thread.sleep(5000);
+        onView(withId(R.id.extrasEditText)).perform(typeText("1000"), closeSoftKeyboard());
+        onView(withId(R.id.button_rent_post)).perform(click());
+        onView(withText(R.string.someone_has_empty_field)).inRoot(withDecorView(not(is(mActivity.getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
 
     @After
@@ -42,3 +64,4 @@ public class RentActivityTest {
         mActivity = null;
     }
 }
+
